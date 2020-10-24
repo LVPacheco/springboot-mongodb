244f4d218aff931d11cadcd922fbbee07e4ad8b4
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-24 09:49:32 -0300

    Find user by id

--- src/main/java/com/pacheco/springbootmongodb/services/UserService.java
+++ src/main/java/com/pacheco/springbootmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.pacheco.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pacheco.springbootmongodb.domain.User;
 import com.pacheco.springbootmongodb.repository.UserRepository;
+import com.pacheco.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
 	}
 
 }
