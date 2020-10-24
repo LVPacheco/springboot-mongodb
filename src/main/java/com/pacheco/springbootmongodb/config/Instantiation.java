dde5bb4ae3badc879715daa7a44dd4c029f9eb31
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-24 14:10:26 -0300

    Find post

--- src/main/java/com/pacheco/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/pacheco/springbootmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.pacheco.springbootmongodb.domain.Post;
 import com.pacheco.springbootmongodb.domain.User;
 import com.pacheco.springbootmongodb.dto.AuthorDTO;
+import com.pacheco.springbootmongodb.dto.CommentDTO;
 import com.pacheco.springbootmongodb.repository.PostRepository;
 import com.pacheco.springbootmongodb.repository.UserRepository;
 
@@ -41,6 +42,14 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf1.parse("18/06/1999"), "Traveling...", "Going to São Paulo", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf1.parse("26/03/1999"), "Good Morning", "Woke up happy today!",new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Have a nice trip", sdf1.parse("02/03/1970"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Enjoy", sdf1.parse("01/05/1970"), new AuthorDTO(alex));
+		CommentDTO c3 = new CommentDTO("Have a nice day", sdf1.parse("06/02/1970"), new AuthorDTO(bob));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c3));
+		post2.getComments().add(c2);
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
--- src/main/java/com/pacheco/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/pacheco/springbootmongodb/repository/PostRepository.java
@@ -1,11 +1,16 @@
 package com.pacheco.springbootmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pacheco.springbootmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
