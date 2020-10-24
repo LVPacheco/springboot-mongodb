e3d36e882cb7720ffdcba7a29e281ce2cea896c5
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-24 10:46:11 -0300

    User insert

--- src/main/java/com/pacheco/springbootmongodb/services/UserService.java
+++ src/main/java/com/pacheco/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pacheco.springbootmongodb.domain.User;
+import com.pacheco.springbootmongodb.dto.UserDTO;
 import com.pacheco.springbootmongodb.repository.UserRepository;
 import com.pacheco.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
